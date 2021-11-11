de5bdcea5ca238bf79d46d783e81376cc6106b8d
mateotiedra <mateo@tiedra.com>
2021-11-11 21:28:35 +0100

    add nodemailer/mailgun

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 var corsOptions = {
   origin: process.env.APP_ORIGIN,
-  methods: ['GET', 'POST', 'DELETE'],
+  methods: ["GET", "POST", "DELETE"],
   credentials: true,
 };
 
@@ -20,12 +20,12 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // database
-const db = require('./app/models');
+const db = require("./app/models");
 
 // force: true will drop the table if it already exists
 if (false) {
   db.sequelize.sync({ force: true }).then(() => {
-    console.log('Drop and Resync Database with { force: true }');
+    console.log("Drop and Resync Database with { force: true }");
     initial();
   });
 } else {
@@ -33,14 +33,14 @@ if (false) {
 }
 
 // simple route
-app.get('/api', (req, res) => {
-  res.json({ message: 'Welcome to okalo api.' });
+app.get("/api", (req, res) => {
+  res.json({ message: "Welcome to okalo api." });
 });
 
 // routes
-require('./app/routes/auth.routes')(app);
-require('./app/routes/user.routes')(app);
-require('./app/routes/bid.routes')(app);
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
+require("./app/routes/bid.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
